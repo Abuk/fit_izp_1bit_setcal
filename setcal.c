f3d417063e7d36a4b4d0a72fd24653ddd8b71b45
jakubschenk <schenk@webscope.io>
2021-11-28 00:36:15 +0100

    WTF: the sequel

--- setcal.c
+++ setcal.c
@@ -3,13 +3,18 @@
 #include <string.h>
 #include "utils/loader.h"
 #include "utils/parser.h"
+#include "utils/types.h"
+
+struct program_params_t program_params;
+struct universe_t universe;
+struct set_t *sets;
+struct relation_t *relations;
 
 int main(int argc, char **argv) {
     FILE *file = NULL;
     char *line = NULL;
-    struct program_params_t program_params;
 
-    if (!program_params_construct(&program_params, argc, argv)) {
+    if (!program_params_construct(argc, argv)) {
         print_program_usage();
         return EXIT_FAILURE;
     }
@@ -17,16 +22,18 @@ int main(int argc, char **argv) {
     if (!load_file(program_params, &file)) {
         return EXIT_FAILURE;
     }
-    program_params_destruct(&program_params);
+    program_params_destruct();
+    universe_construct();
 
     while (get_line(file, &line)) {
-        if(strlen(line) != 0) {
-            printf("%s\n", line);
+        if (strlen(line) != 0) {
+            if(!parse_line(line)) {
+                return EXIT_FAILURE;
+            }
         }
     }
 
-
-
+    universe_destruct(&universe);
     fclose(file);
     free(line);
     line = NULL;
--- utils/parser.h
+++ utils/parser.h
@@ -5,6 +5,13 @@
 #ifndef PROJECT_02_PARSER_H
 #define PROJECT_02_PARSER_H
 
+#include "types.h"
+
+extern struct program_params_t program_params;
+extern struct universe_t universe;
+extern struct set_t *sets;
+extern struct relation_t *relations;
+
 struct program_params_t {
     char *program_path;
     char *file_path;
@@ -12,10 +19,15 @@ struct program_params_t {
 
 void print_program_usage();
 
-int program_params_construct(struct program_params_t *params, int argv, char **argc);
+int program_params_construct(int argv, char **argc);
 
-void program_params_destruct(struct program_params_t *params);
-//int parse_line(struct node)
+void program_params_destruct();
+
+int parse_line(char *line);
+
+int parse_universe(char *universe_string);
+
+int parse_set(int set_pos, char *line);
 
 
 #endif //PROJECT_02_PARSER_H
